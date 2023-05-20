c978fdc98b670810b06ad7ed88a3079399cf0d78
GraydonvG <100849896+GraydonvG@users.noreply.github.com>
2023-05-20 14:43:05 +0200

    Update store.js
    fixing typo

--- src/store/store.js
+++ src/store/store.js
@@ -18,7 +18,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }).concat(logger),
 });
